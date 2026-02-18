a876262d21e5ace4e081f34380bc7931fb7988db
Abhijeet <260019722+abhijeetlovesmahi-commits@users.noreply.github.com>
2026-02-18 13:17:19 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyDqDmsMp2eAuHJBcjW-ciO2JcLTXapiIrs",
   authDomain: "the-lalit-d7472.firebaseapp.com",
@@ -5,9 +6,6 @@ const firebaseConfig = {
   appId: "1:479237084229:web:31078825739b3c5712ff2c"
 };
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
-var db = firebase.firestore();
-var auth = firebase.auth();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
