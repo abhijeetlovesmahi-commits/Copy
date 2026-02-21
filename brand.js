8b4542e20bd276c2834152762bd46533cb265a13
Abhijeet <260019722+abhijeetlovesmahi-commits@users.noreply.github.com>
2026-02-21 19:58:24 +0530

    Update brand.js

--- brand.js
+++ brand.js
@@ -4,7 +4,7 @@ function applyBranding() {
     if (brandContainer) {
         brandContainer.innerHTML = `
             <img src="logo.png" alt="School Logo" class="school-logo" onerror="this.src='https://via.placeholder.com/110'">
-            <h1>The Lalit International School</h1>
+            <h1>VIDYA International School</h1>
             <p style="letter-spacing: 5px; font-size: 0.8rem; color: var(--deep-gold);">NURTURING GLOBAL CITIZENS</p>
         `;
     }
